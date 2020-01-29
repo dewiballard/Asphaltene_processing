e930772350533f88431df313b0a1152bfec6de31
dewiballard <dewiballard@hotmail.com>
2020-01-29 23:57:41 +0000

    Completed violin plots

--- Violin_plots/plot.py
+++ Violin_plots/plot.py
@@ -21,39 +21,24 @@ def new_hue(x):
     else:
         return ["RA", "IAA"]
 
+sns.set(style='white', font_scale=1.5)
 g = plt.subplots(figsize=(15,10))
-
-#for system1 in order:
-#    for system2 in hue_order:
-#        grouped1 = df.groupby('group').get_group(system1)
-#        grouped2 = df.groupby('sample').get_group(system2)
-#        grouped = pd.merge(grouped1, grouped2)
-#        fig = sns.violinplot(x=grouped['group'], y=grouped['total'], hue=df['sample'], order=order, hue_order=hue_order, split=True, width=grouped['vdW'].mean()+grouped['electro'].mean()+grouped['hbond'].mean(), inner=None, palette=['blue','blue'])
-#        fig = sns.violinplot(x=grouped['group'], y=grouped['total'], hue=df['sample'], order=order, hue_order=hue_order, split=True, width=grouped['electro'].mean()+grouped['hbond'].mean(), inner=None, palette=['yellow','yellow'])
-#        fig = sns.violinplot(x=grouped['group'], y=grouped['total'], hue=df['sample'], order=order, hue_order=hue_order, split=True, width=grouped['hbond'].mean(), inner=None, palette=['red','red'])
 
 for system1 in order:
     for system2 in hue_order:
         grouped1 = df.groupby('group').get_group(system1)
         grouped2 = df.groupby('sample').get_group(system2)
         grouped = pd.merge(grouped1, grouped2)
-#        if df['sample'] = 'RA':
-#            new_hue_order = ["IAA", "RA"]
-#        else:
-#            new_hue_order = ["RA", "IAA"]
         fig = sns.violinplot(x=grouped['group'], y=grouped['total'], hue=df['sample'], order=order, hue_order=new_hue(grouped['sample']), split=True, width=grouped['vdW'].mean()+grouped['electro'].mean()+grouped['hbond'].mean(), inner=None, palette=['blue','blue'])
         fig = sns.violinplot(x=grouped['group'], y=grouped['total'], hue=df['sample'], order=order, hue_order=new_hue(grouped['sample']), split=True, width=grouped['electro'].mean()+grouped['hbond'].mean(), inner=None, palette=['yellow','yellow'])
         fig = sns.violinplot(x=grouped['group'], y=grouped['total'], hue=df['sample'], order=order, hue_order=new_hue(grouped['sample']), split=True, width=grouped['hbond'].mean(), inner=None, palette=['red','red'])
 
-#fig = sns.violinplot(x=df['group'], y=df['total'], hue=df['sample'],order=order,  split=True, width=1, inner=None, palette=['blue','red'])
-fig.set_title('Interactions with itself', fontsize=20)
-
+plt.style.use('classic')
+fig.set_title('Interactions with toluene', fontsize=20)
 fig.set_xlabel('Heteroatom group', fontsize=20)
-
 fig.set_ylabel('Interaction energy (kcal/mol)', fontsize=20)
 
 custom_legend = [Line2D([0], [0], color="blue", lw=4), Line2D([0], [0], color="yellow", lw=4), Line2D([0], [0], color="red", lw=4)]
-
 fig.legend(custom_legend, ['van der Waals', 'Electrostatic', 'Hydrogen bonding'], loc='lower left')
 
 plt.savefig('out.png')
---
+++ Violin_plots/scaling_for_final_plot.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 29 23:40:27 2020
+
+@author: dewiballard
+"""
+
+import os
+
+csv_header = '0,sample,group,vdW,hbond,electro,total'
+csv_out = 'Scaled_data.csv'
+
+tot1 = (32+11+13+12+23)
+RA_HC = int((32/tot1)*100)
+RA_Nx = int((11/tot1)*100)
+RA_Ox = int((13/tot1)*100)
+RA_OxSy = int((12/tot1)*100)
+RA_Sx = int((23/tot1)*100)
+
+tot2 = (22+8+27+25+11)
+IAA_HC = int((22/tot2)*100)
+IAA_NxOy = int((8/tot2)*100)
+IAA_Ox = int((27/tot2)*100)
+IAA_OxSy = int((25/tot2)*100)
+IAA_Sx = int((11/tot2)*100)
+
+csv_dir = os.getcwd()
+
+dir_tree = os.walk(csv_dir)
+for dirpath, dirnames, filenames in dir_tree:
+   pass
+
+csv_list = []
+for file in filenames:
+   if file.endswith('data.csv'):
+      csv_list.append(file)
+
+csv_processed = open(csv_out, 'w')
+csv_processed.write(csv_header)
+csv_processed.write('\n')
+
+for file in csv_list:
+   csv_in = open(file)
+   for line in csv_in:
+       if line.startswith(csv_header):
+           continue
+       if 'R' in line:
+           if 'HC' in line:
+               for i in range(RA_HC):
+                   csv_processed.write(line)
+           if 'Nx' in line:
+               for i in range(RA_Nx):
+                   csv_processed.write(line)
+           if 'Ox,' in line:
+               for i in range(RA_Ox):
+                   csv_processed.write(line)
+           if 'OxSy' in line:
+               for i in range(RA_OxSy):
+                   csv_processed.write(line)
+           if 'Sx' in line:
+               for i in range(RA_Sx):
+                   csv_processed.write(line)
+       elif 'I' in line:
+           if 'HC' in line:
+               for i in range(IAA_HC):
+                   csv_processed.write(line)
+           if 'Nx' in line:
+               for i in range(IAA_NxOy):
+                   csv_processed.write(line)
+           if 'Ox,' in line:
+               for i in range(IAA_Ox):
+                   csv_processed.write(line)
+           if 'OxSy' in line:
+               for i in range(IAA_OxSy):
+                   csv_processed.write(line)
+           if 'Sx' in line:
+               for i in range(IAA_Sx):
+                   csv_processed.write(line)
+       else:
+          pass
+   
+   csv_in.close()
+
+csv_processed.close()
